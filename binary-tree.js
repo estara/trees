b465fc761a998835e7c95709fdaef9388e582c5c
Estara <heatherjpdx@gmail.com>
2021-04-29 19:54:04 -0700

    finished

--- binary-tree.js
+++ binary-tree.js
@@ -17,28 +17,92 @@ class BinaryTree {
    * the length of the shortest path from the root to a leaf. */
 
   minDepth() {
+    if (this.root === null) {return 0}
 
+    let traverse = node => {
+      if (node.left === null && node.right === null) {return 1}
+      if (node.left === null) {
+        return traverse(node.right)+1;
+      }
+      if (node.right === null) {
+        return traverse(node.left)+1;
+      }
+      return (Math.min(traverse(node.left), traverse(node.right))+1)
+    };
+    
+    return traverse(this.root)
   }
 
   /** maxDepth(): return the maximum depth of the tree -- that is,
    * the length of the longest path from the root to a leaf. */
 
   maxDepth() {
+    if (this.root === null) {return 0}
+    let current = this.root;
+    let lLength = 1;
+    let rLength = 1;
+    let traverse = node => {
+      
+      if (node.left) {
+        if (node === this.root) {
+          lLength = 1;
+        }
+        lLength += 1;
+        traverse(node.left);
+      }
+      if (node.right) {
+        if (node === this.root) {
+          rLength = 1;
+        }
+        rLength += 1
+        traverse(node.right);
+      }
+    };
 
+    traverse(current);
+    if (rLength < lLength) {return lLength}
+    return rLength
   }
 
   /** maxSum(): return the maximum sum you can obtain by traveling along a path in the tree.
    * The path doesn't need to start at the root, but you can't visit a node more than once. */
 
   maxSum() {
-
+    if (this.root === null) {return 0}
+    let result = 0;
+    
+    let traverse = node => {
+      if (node === null) {return 0}
+      let left = traverse(node.left);
+      let right = traverse(node.right);
+      result = Math.max(result, node.val + left + right)
+      return Math.max(0, left + node.val, right + node.val);
+      }
+    traverse(this.root);
+    
+    return result
   }
 
   /** nextLarger(lowerBound): return the smallest value in the tree
    * which is larger than lowerBound. Return null if no such value exists. */
 
   nextLarger(lowerBound) {
+    let visited = null;
+    let current = this.root;
 
+    let traverse = node => {
+      if (node !== null) {
+        if (node.val > lowerBound && (node.val < visited || visited === null)) {
+          visited = node.val;
+        }
+        if (node.left) traverse(node.left);
+        if (node.right) traverse(node.right);
+      };
+    }
+
+    traverse(current);
+    
+    return visited;
   }
 
   /** Further study!
--- tree.js
+++ tree.js
@@ -15,20 +15,67 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
+    let total = 0;
+    let queue = [];
+    queue.push(this.root);
     
+    while (queue.length) {
+      let current = queue.shift();
+      if (current) {
+        total += current.val;
+        for (let child of current.children) {
+          queue.push(child);
+        }
+      }
+      
+    };
+    return total;
+
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
-
+    let total = 0;
+    let queue = [];
+    queue.push(this.root);
+    
+    while (queue.length) {
+      let current = queue.shift();
+      if (current) {
+        if (current.val % 2 === 0) {
+        total += 1;
+        }
+        for (let child of current.children) {
+          queue.push(child);
+        }
+      }
+      
+    };
+    return total;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
-
+    let total = 0;
+    let queue = [];
+    queue.push(this.root);
+    
+    while (queue.length) {
+      let current = queue.shift();
+      if (current) {
+        if (current.val > lowerBound) {
+        total += 1;
+        }
+        for (let child of current.children) {
+          queue.push(child);
+        }
+      }
+      
+    };
+    return total;
   }
 }
 
